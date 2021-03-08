c51b71a18dc56ad3e96ebf89b6adeb92efffddf3
Kwon-YJ <kyj1435@gmail.com>
2021-03-08 12:58:24 +0900

    1

--- Pincette/convert_img.py
+++ Pincette/convert_img.py
@@ -4,9 +4,9 @@ from PIL import Image
 
 im = Image.open("sample.png")
 
-#im.save("sample_bmp.bmp")
+im.save("sample_bmp.bmp")
 
-#im.save("sample_png.png")
+im.save("sample_png.png")
 
 im.save("sample_gif.gif")
 
