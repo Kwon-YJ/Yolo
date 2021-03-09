a1072c46f6519e2f3943588325502ce4d330e36f
Kwon-YJ <kyj1435@gmail.com>
2021-03-09 17:13:54 +0900

    1

--- OPENCV_study/4_1.py
+++ OPENCV_study/4_1.py
@@ -13,8 +13,14 @@ lineType: 선 그리기 형식 (cv2.LINE_4, cv2.LINE_8, cv2.LINE_AA)
 '''
 
 import cv2
-img = cv2.imread('img_file/blank_500.jpg')
+img = cv2.imread('img_file/blank_500.jpeg')
 
-cv2.line(img, (50, 50), (150, 50), (250,0,0)) # 
+cv2.line(img, (50, 50), (150, 50), (255,0,0))   # 파란색 1픽셀 선
+cv2.line(ing,(200,50), (300,50), (0,255,0)) # 초록색 1픽셀 선
 
 
+
+
+cv2.imshow('lines', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
