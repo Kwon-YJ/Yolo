0fdf8f30cbad58cd18b1fe071d5911f6db880fa9
Kwon-YJ <49362903+Kwon-YJ@users.noreply.github.com>
2021-03-13 20:59:32 +0900

    1

---
+++ OPENCV_study/8-2.py
@@ -0,0 +1,17 @@
+# 스레시홀딩 플래그
+
+import cv2
+import numpy as np
+import matplotlib.pylab as plt
+
+img = cv2.imread('img_file/gray_gradient.jpg', cv2.IMREAD_GRAYSCALE)
+
+_, t_bin = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+
+_, t_bin = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+
+_, t_bin = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+
+_, t_bin = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+
+_, t_bin = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
